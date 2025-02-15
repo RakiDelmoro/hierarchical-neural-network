5bbd605a6517b43da1d88c572fd38f71daba5b44
Raki Delmoro <rakidelmoro@gmail.com>
2025-02-15 20:36:55 +0800

    Incremental predictive coding model

--- IPC/model.py
+++ IPC/model.py
@@ -1,8 +1,17 @@
 import math
 import torch
+import random
 import numpy as np
 from torch.nn import init
 from torch.nn.init import kaiming_uniform_
+from features import RED, GREEN, RESET
+
+def tanh(input_data, return_derivative=False):
+    if return_derivative:
+        input_data = (np.exp(input_data) - np.exp(-input_data))/(np.exp(input_data) + np.exp(-input_data))
+        return 1 - input_data * input_data
+    else:
+        return (np.exp(input_data) - np.exp(-input_data))/(np.exp(input_data) + np.exp(-input_data))
 
 def initializer(input_size, output_size):
     gen_w_matrix = torch.empty(size=(input_size, output_size))
@@ -22,22 +31,22 @@ def parameters_init(network_architecture: list):
         parameters.append(connections)
     return parameters
 
-def calculate_activation_error(forward_activations, backward_activations):
-    # Mean squared error for each layer activation
-    error = 0
-    layers_neurons_error = []
-    for each in range(len(forward_activations)-1):
-        error += np.mean((forward_activations[each+1] - backward_activations[-(each+1)])**2)
-        # Derivative of mean squared error 
-        layer_error = 2 * (forward_activations[each+1] - backward_activations[-(each+1)])
-        layers_neurons_error.append(layer_error)
-    return error, layers_neurons_error
-
-def refine_activation(activations, activations_loss, learning_rate):
+def refine_activations(activations, activations_loss, parameters, learning_rate):
     new_activations = []
-    for each in range(len(activations)):
-        new_activation = activations[each] - (learning_rate * activations_loss[-(each+1)])
+    for each in range(len(activations_loss)):
+        weights = parameters[-each][0].T
+        if each == 0:
+            # Zero term since we don't have a previous layer in last layer
+            delta_x =  0.5 * (-activations_loss[each] + 0)
+        else:
+            propagated_error = np.matmul(activations_loss[each-1], weights)
+            term = tanh(activations[-(each+1)], return_derivative=True) * propagated_error
+            delta_x = 0.5 * (-activations_loss[each] + term)
+
+        new_activation = activations[-(each+1)] + delta_x
         new_activations.append(new_activation)
+    new_activations.append(activations[0])
+    # From output to input -> input to output
     return new_activations
 
 def backward_pass(forward_pass_output, label, parameters):
@@ -45,10 +54,9 @@ def backward_pass(forward_pass_output, label, parameters):
     activation = forward_pass_output
     # top to bottom
     reversed_parameters = parameters[::-1]
-    for each in range(len(reversed_parameters)-1):
+    for each in range(len(reversed_parameters)):
         weights = reversed_parameters[each][0]
-        bias = reversed_parameters[each][1]
-        activation = np.matmul(activation, weights.T) + bias
+        activation = np.matmul(activation, weights.T)
         activations.append(activation)
     return activations
 
@@ -56,33 +64,72 @@ def forward_pass(input_data, parameters):
     activations = [input_data]
     activation = input_data
     for each in range(len(parameters)):
+        last_layer = len(parameters)-2
         weights = parameters[each][0]
-        bias = parameters[each][1]
-        activation = np.matmul(activation, weights) + bias
+        pre_activation = np.matmul(activation, weights)
+        activation = tanh(pre_activation) if last_layer else pre_activation
         activations.append(activation)
     return activations
 
-def update_connection(activations):
-    # TODO: Update the connection weights
-    pass
+def calculate_activation_error(forward_activations, label, parameters):
+    activation_errors = [forward_activations[-1] - label]
 
-def update_parameters(forward_activations, predicted_activations, num_iterations, learning_rate, parameters):
-    for _ in range(num_iterations):
-        error, layers_activation_error = calculate_activation_error(forward_activations, predicted_activations)
-        refine_activations = refine_activation(predicted_activations, layers_activation_error, learning_rate)
-        update_connection(refine_activations)
+    reversed_activations = forward_activations[::-1]
+    for each in range(len(reversed_activations)-2):
+        predicted_activation = tanh(np.matmul(reversed_activations[each], parameters[-(each+1)][0].T))
+        error = predicted_activation - reversed_activations[each+1]
+        activation_errors.append(error)
+    return activation_errors
+
+def update_connection(activations, activations_error, parameters):
+    for each in range(len(parameters)):
+        weights = parameters[-(each+1)][0]
+
+        activation = activations[each+1]
+        activation_error = activations_error[each]
+
+        hebbs_rule = (0.0001 * np.matmul(activation.T, activation_error)) / len(activation)
+        weights -= hebbs_rule
+
+def update_parameters(forward_activations, expected, num_iterations, learning_rate, parameters):
+    for i in range(num_iterations):
+        layers_activation_error = calculate_activation_error(forward_activations, expected, parameters)
+        predicted_activations_refined = refine_activations(forward_activations, layers_activation_error, parameters, learning_rate)
+        update_connection(predicted_activations_refined, layers_activation_error, parameters)
+        print(f'Iters: {i} Loss: {np.mean((predicted_activations_refined[0] - expected)**2)}\r', end='', flush=True)
 
 def neural_network(size: list):
     parameters = parameters_init(size)
 
     def train_runner(dataloader):
+        losses = []
         for input_image, label in dataloader:
             forward_activations = forward_pass(input_image, parameters)
-            predicted_activations = backward_pass(forward_activations[-1], label, parameters)
-            update_parameters(forward_activations, predicted_activations, 10, 0.1, parameters)
+            update_parameters(forward_activations, label, 50, 0.1, parameters)
+            loss = np.mean((forward_activations[-1] - label)**2)
+            losses.append(loss)
 
-    return train_runner
+        return np.mean(np.array(losses))
+    
+    def test_runner(dataloader):
+        accuracy = []
+        correctness = []
+        wrongness = []
+        for i, (batched_image, batched_label) in enumerate(dataloader):
+            neurons_activations = forward_pass(batched_image, parameters)
+            batch_accuracy = (neurons_activations[-1].argmax(axis=-1) == batched_label.argmax(axis=-1)).mean()
+            for each in range(len(batched_label)//10):
+                model_prediction = neurons_activations[-1][each].argmax()
+                if model_prediction == batched_label[each].argmax(axis=-1): correctness.append((model_prediction.item(), batched_label[each].argmax(axis=-1).item()))
+                else: wrongness.append((model_prediction.item(), batched_label[each].argmax(axis=-1).item()))
+            print(f'Number of samples: {i+1}\r', end='', flush=True)
+            accuracy.append(np.mean(batch_accuracy))
+        random.shuffle(correctness)
+        random.shuffle(wrongness)
+        print(f'{GREEN}Model Correct Predictions{RESET}')
+        [print(f"Digit Image is: {GREEN}{expected}{RESET} Model Prediction: {GREEN}{prediction}{RESET}") for i, (prediction, expected) in enumerate(correctness) if i < 5]
+        print(f'{RED}Model Wrong Predictions{RESET}')
+        [print(f"Digit Image is: {RED}{expected}{RESET} Model Prediction: {RED}{prediction}{RESET}") for i, (prediction, expected) in enumerate(wrongness) if i < 5]
+        return np.mean(np.array(accuracy)).item()
 
-input_x = [(np.random.rand(1, 3), np.random.rand(1, 1))] * 10
-network = neural_network([3, 5, 1])
-print(network(input_x)[-1])
+    return train_runner, test_runner
