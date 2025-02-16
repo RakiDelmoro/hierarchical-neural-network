b8e3ecee2c164335218469064ca9790207b329a2
Raki Delmoro <rakidelmoro@gmail.com>
2025-02-16 18:41:15 +0800

    Add softmax for output nodes

--- IPC/model.py
+++ IPC/model.py
@@ -12,6 +12,16 @@ def sigmoid(input_data, return_derivative=False):
        return input_data * (1 - input_data)
     else:
         return 1.0 / (1.0+np.exp(-input_data))
+    
+def softmax(input_data):
+    # Subtract max value for numerical stability
+    shifted_data = input_data - np.max(input_data, axis=-1, keepdims=True)
+    # Calculate exp
+    exp_data = np.exp(shifted_data)
+    # Sum along axis=1 and keep dimensions for broadcasting
+    sum_exp_data = np.sum(exp_data, axis=-1, keepdims=True)
+
+    return exp_data / sum_exp_data
 
 def initializer(input_size, output_size):
     gen_w_matrix = torch.empty(size=(input_size, output_size))
@@ -34,32 +44,20 @@ def parameters_init(network_architecture: list):
 def refine_activations(activations, activations_loss, parameters, learning_rate):
     new_activations = []
     for each in range(len(activations_loss)-1):
-
-        weights = parameters[-(each+1)][0]
-        propagated_error = np.matmul(activations_loss[-(each+2)], weights)
-        deriv_activation = activations[-(each+1)] if each == 0 else sigmoid(activations[-(each+1)], return_derivative=True)
         if each == 0:
-            term = propagated_error
+            delta_x = 0.5 * (-activations_loss[-(each+1)])
         else:
+            weights = parameters[-each][0].T
+            propagated_error = np.matmul(activations_loss[-each], weights)
+            deriv_activation = sigmoid(activations[-(each+1)], return_derivative=True)
             term = deriv_activation * propagated_error
-        delta_x = (0.5 * (-activations_loss[-(each+1)] + term))
-        new_activation = activations[-(each+1)] + delta_x
+            delta_x =  0.5 * (-activations_loss[-(each+1)] + term)
+        new_activation = activations[-(each+1)] + delta_x 
         new_activations.append(new_activation)
 
     new_activations.append(activations[0])
     # From output to input -> input to output
     return new_activations[::-1]
-
-def backward_pass(forward_pass_output, label, parameters):
-    activations = [label]
-    activation = forward_pass_output
-    # top to bottom
-    reversed_parameters = parameters[::-1]
-    for each in range(len(reversed_parameters)):
-        weights = reversed_parameters[each][0]
-        activation = np.matmul(activation, weights.T)
-        activations.append(activation)
-    return activations
 
 def forward_pass(input_data, parameters):
     activations = [input_data]
@@ -68,38 +66,40 @@ def forward_pass(input_data, parameters):
         last_layer = each == len(parameters)-1
         weights = parameters[each][0]
         pre_activation = np.matmul(activation, weights)
-        activation = sigmoid(pre_activation) if not last_layer else pre_activation
+        activation = sigmoid(pre_activation) if not last_layer else softmax(pre_activation)
         activations.append(activation)
     return activations
 
 def calculate_activation_error(forward_activations, label, parameters):
     loss = np.mean((forward_activations[-1] - label)**2)
-    activation_errors = []
+    activation_errors = [forward_activations[-1] - label]
 
-    for each in range(len(forward_activations)-1):
-        # Prediction from top layer 
-        predicted_activation = np.matmul(forward_activations[each+1], parameters[each][0].T)
-        error = predicted_activation - forward_activations[each]
+    for each in range(len(forward_activations)-2):
+        # Prediction from top layer to bottom layer
+        predicted_activation = np.matmul(forward_activations[-(each+1)], parameters[-(each+1)][0].T)
+        error = predicted_activation - forward_activations[-(each+2)]
         loss += np.mean(error**2)
         activation_errors.append(error)
-    activation_errors.append(forward_activations[-1] - label)
     return loss, activation_errors
 
-def update_connection(activations, activations_error, parameters):
+def update_connection(activations, label, activations_error, parameters):
     for each in range(len(parameters)):
         weights = parameters[-(each+1)][0]
 
-        activation = activations[-(each+2)]
-        activation_error = activations_error[-(each+1)]
-
-        hebbs_rule = (0.00001 * np.matmul(activation.T, activation_error) / activation.shape[0])
-        weights -= hebbs_rule
+        if each == 0:
+            activation = label
+        else:
+            activation = activations[-(each+1)]
+        
+        activation_error = activations_error[-(each+2)]
+        hebbs_rule = (0.000001 * np.matmul(activation.T, activation_error) / activation.shape[0])
+        weights -= hebbs_rule.T
 
 def update_parameters(forward_activations, expected, num_iterations, learning_rate, parameters):
     # for i in range(num_iterations):
     loss, layers_activation_error = calculate_activation_error(forward_activations, expected, parameters)
     predicted_activations_refined = refine_activations(forward_activations, layers_activation_error, parameters, learning_rate)
-    update_connection(predicted_activations_refined, layers_activation_error, parameters)
+    update_connection(predicted_activations_refined, expected, layers_activation_error, parameters)
     print(f'Loss: {loss}\r', end='', flush=True)
 
 def neural_network(size: list):
