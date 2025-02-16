038a7738f2e86409f538a383a0de46e923cf41f5
Raki Delmoro <rakidelmoro@gmail.com>
2025-02-16 12:35:03 +0800

    Forward pass fix bug not correct of applying activation function

--- IPC/model.py
+++ IPC/model.py
@@ -6,12 +6,12 @@ from torch.nn import init
 from torch.nn.init import kaiming_uniform_
 from features import RED, GREEN, RESET
 
-def tanh(input_data, return_derivative=False):
+def sigmoid(input_data, return_derivative=False):
     if return_derivative:
-        input_data = (np.exp(input_data) - np.exp(-input_data))/(np.exp(input_data) + np.exp(-input_data))
-        return 1 - input_data * input_data
+       input_data = 1.0 / (1.0+np.exp(-input_data))
+       return input_data * (1 - input_data)
     else:
-        return (np.exp(input_data) - np.exp(-input_data))/(np.exp(input_data) + np.exp(-input_data))
+        return 1.0 / (1.0+np.exp(-input_data))
 
 def initializer(input_size, output_size):
     gen_w_matrix = torch.empty(size=(input_size, output_size))
@@ -33,21 +33,22 @@ def parameters_init(network_architecture: list):
 
 def refine_activations(activations, activations_loss, parameters, learning_rate):
     new_activations = []
-    for each in range(len(activations_loss)):
-        weights = parameters[-each][0].T
-        if each == 0:
-            # Zero term since we don't have a previous layer in last layer
-            delta_x =  (0.5 * (-activations_loss[each] + 0))
-        else:
-            propagated_error = np.matmul(activations_loss[each-1], weights)
-            term = tanh(activations[-(each+1)], return_derivative=True) * propagated_error
-            delta_x = (0.5 * (-activations_loss[each] + term))
+    for each in range(len(activations_loss)-1):
 
+        weights = parameters[-(each+1)][0]
+        propagated_error = np.matmul(activations_loss[-(each+2)], weights)
+        deriv_activation = activations[-(each+1)] if each == 0 else sigmoid(activations[-(each+1)], return_derivative=True)
+        if each == 0:
+            term = propagated_error
+        else:
+            term = deriv_activation * propagated_error
+        delta_x = (0.5 * (-activations_loss[-(each+1)] + term))
         new_activation = activations[-(each+1)] + delta_x
         new_activations.append(new_activation)
+
     new_activations.append(activations[0])
     # From output to input -> input to output
-    return new_activations
+    return new_activations[::-1]
 
 def backward_pass(forward_pass_output, label, parameters):
     activations = [label]
@@ -64,41 +65,42 @@ def forward_pass(input_data, parameters):
     activations = [input_data]
     activation = input_data
     for each in range(len(parameters)):
-        last_layer = len(parameters)-2
+        last_layer = each == len(parameters)-1
         weights = parameters[each][0]
         pre_activation = np.matmul(activation, weights)
-        activation = tanh(pre_activation) if last_layer else pre_activation
+        activation = sigmoid(pre_activation) if not last_layer else pre_activation
         activations.append(activation)
     return activations
 
 def calculate_activation_error(forward_activations, label, parameters):
     loss = np.mean((forward_activations[-1] - label)**2)
-    activation_errors = [forward_activations[-1] - label]
+    activation_errors = []
 
-    reversed_activations = forward_activations[::-1]
-    for each in range(len(reversed_activations)-2):
-        predicted_activation = tanh(np.matmul(reversed_activations[each], parameters[-(each+1)][0].T))
-        error = predicted_activation - reversed_activations[each+1]
+    for each in range(len(forward_activations)-1):
+        # Prediction from top layer 
+        predicted_activation = np.matmul(forward_activations[each+1], parameters[each][0].T)
+        error = predicted_activation - forward_activations[each]
         loss += np.mean(error**2)
         activation_errors.append(error)
+    activation_errors.append(forward_activations[-1] - label)
     return loss, activation_errors
 
 def update_connection(activations, activations_error, parameters):
     for each in range(len(parameters)):
         weights = parameters[-(each+1)][0]
 
-        activation = activations[each+1]
-        activation_error = activations_error[each]
+        activation = activations[-(each+2)]
+        activation_error = activations_error[-(each+1)]
 
-        hebbs_rule = (0.0001 * np.matmul(activation.T, activation_error) / activation.shape[0])
+        hebbs_rule = (0.00001 * np.matmul(activation.T, activation_error) / activation.shape[0])
         weights -= hebbs_rule
 
 def update_parameters(forward_activations, expected, num_iterations, learning_rate, parameters):
-    for i in range(num_iterations):
-        loss, layers_activation_error = calculate_activation_error(forward_activations, expected, parameters)
-        predicted_activations_refined = refine_activations(forward_activations, layers_activation_error, parameters, learning_rate)
-        update_connection(predicted_activations_refined, layers_activation_error, parameters)
-        print(f'Iters: {i} Loss: {loss}\r', end='', flush=True)
+    # for i in range(num_iterations):
+    loss, layers_activation_error = calculate_activation_error(forward_activations, expected, parameters)
+    predicted_activations_refined = refine_activations(forward_activations, layers_activation_error, parameters, learning_rate)
+    update_connection(predicted_activations_refined, layers_activation_error, parameters)
+    print(f'Loss: {loss}\r', end='', flush=True)
 
 def neural_network(size: list):
     parameters = parameters_init(size)
