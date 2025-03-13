785cd573e83ed18e69f1f641600ebc0306b588f9
Raki Delmoro <rakidelmoro@gmail.com>
2025-03-13 10:49:34 +0800

    preliminary backpropagation

--- dpc_numpy.py
+++ dpc_numpy.py
@@ -32,7 +32,7 @@ def lower_network_forward(input_data, parameters, caches):
     activation = np.matmul(input_data, parameters.T)
     activations.append(activation)
 
-    caches['lower_network_caches'] = activations
+    caches['lower_network_caches'].append(activations)
 
     return activation
 
@@ -48,7 +48,7 @@ def hyper_network_forward(input_data, parameters, caches):
         activation = pre_activation if last_layer else relu(pre_activation)
         activations.append(activation)
     
-    caches['hyper_network_caches'] = activations
+    caches['hyper_network_caches'].append(activations)
 
     return activation
 
@@ -67,9 +67,19 @@ def rnn_forward(input_data, hidden_state, parameters, caches):
     input_to_hidden_caches.append(input_to_hidden_activation)
     hidden_to_hidden_caches.append(hidden_to_hidden_activation)
 
-    caches['rnn_caches'] = [input_to_hidden_caches, hidden_to_hidden_caches]
+    caches['rnn_caches'].append([input_to_hidden_caches, hidden_to_hidden_caches])
 
     return output
+
+def combine_transitions(weights, Vk_parameters, caches):
+    combined_transitions = []
+    for k in range(len(Vk_parameters)):
+        transition = weights[:, k].reshape(-1, 1, 1) * Vk_parameters[k]
+        combined_transitions.append(transition)
+
+    caches['combine_transitions_caches'].append(sum(combined_transitions))
+
+    return sum(combined_transitions)
 
 def classifier_forward(input_data, parameters, caches):
     activations = [input_data]
@@ -79,14 +89,40 @@ def classifier_forward(input_data, parameters, caches):
     activation = np.matmul(input_data, weights.T) + bias
     activations.append(activation)
 
-    caches['classifier_caches'] = activations
+    caches['classifier_caches'].append(activations)
 
     return activation
 
-def backpropagate(caches, gradients, prediction_error):
-    pass
+def prediction_frame_error(predicted, expected, caches):
+    error = predicted - expected
+    caches['prediction_error'].append(error)
 
-def numpy_train_runner(model, dataloader):
+    return np.mean(error**2), error
+
+def backpropagate(batched_image, caches, gradients, torch_model):
+    # Dimensions
+    batch_size, seq_len, _ = batched_image.shape
+    K = torch_model.K
+    lower_dim = torch_model.lower_dim
+    higher_dim = torch_model.higher_dim
+
+    # Numpy model activations caches
+    lower_network_act = caches['lower_network_caches']
+    hyper_network_act = caches['hyper_network_caches']
+    rnn_act = caches['rnn_caches']
+    combine_transitions_act = caches['combine_transitions_caches']
+    classifier_act = caches['classifier_caches']
+    prediction_error = caches['prediction_error']
+
+    # Initialize gradients
+    lower_level_network_gradients = np.zeros_like(torch_model.lower_level_network.weight.data.numpy())
+    Vk_gradients = [np.zeros_like(vk.detach().numpy()) for vk in torch_model.Vk]
+    hyper_network_gradients = [[np.zeros_like(layer.weight.data.numpy()), np.zeros_like(layer.bias.data.numpy())] for layer in torch_model.hyper_network if isinstance(layer, nn.Linear)][1]
+    higher_rnn_gradients = [[np.zeros_like(torch_model.higher_rnn.weight_ih.data.numpy()), np.zeros_like(torch_model.higher_rnn.bias_ih.data.numpy())],
+                             [np.zeros_like(torch_model.higher_rnn.weight_hh.data.numpy()), np.zeros_like(torch_model.higher_rnn.bias_hh.data.numpy())]]
+    digit_classifier_gradients = [np.zeros_like(torch_model.digit_classifier.weight.data.numpy()), np.zeros_like(torch_model.digit_classifier.bias.data.numpy())]
+
+def numpy_train_runner(model, dataloader, torch_model):
     for batched_image, batched_label in dataloader:
         # From (Batch, height*width) to (Batch, seq_len, height*width)
         batched_image = batched_image.view(batched_image.size(0), -1, 28*28).repeat(1, 5, 1).numpy()
@@ -99,7 +135,7 @@ def numpy_train_runner(model, dataloader):
         # Calculate losses
         loss, gradients = cross_entropy_loss(digit_prediction, batched_label)
 
-        backpropagate(model_activations, gradients, prediction_error)
+        backpropagate(batched_image, model_activations, gradients, torch_model)
         
         # Combine losses with regularization
         loss = loss + 0.1 * prediction_error #+ 0.01 * np.mean(lower_level_network_parameters**2)
@@ -108,6 +144,8 @@ def numpy_train_runner(model, dataloader):
 
 def numpy_dpc(torch_model):
     """Shared parameters initialization with torch model"""
+
+    forward_caches = {'lower_network_caches': [],'hyper_network_caches': [], 'rnn_caches': [], 'combine_transitions_caches': [], 'classifier_caches': [], 'prediction_error': []}
 
     # Spatial decoder
     lower_level_network_parameters = torch_model.lower_level_network.weight.data.numpy()
@@ -119,8 +157,6 @@ def numpy_dpc(torch_model):
     higher_rnn_parameters = [[torch_model.higher_rnn.weight_ih.data.numpy(), torch_model.higher_rnn.bias_ih.data.numpy()], [torch_model.higher_rnn.weight_hh.data.numpy(), torch_model.higher_rnn.bias_hh.data.numpy()]]
     # Digit classifier only
     digit_classifier_parameters = [torch_model.digit_classifier.weight.data.numpy(), torch_model.digit_classifier.bias.data.numpy()]
-
-    forward_caches = {}
 
     def forward(batched_image):
         batch_size, seq_len, _ = batched_image.shape
@@ -134,18 +170,17 @@ def numpy_dpc(torch_model):
         digit_logits = []
 
         for t in range(seq_len):
-            predicted_frame = lower_network_forward(lower_level_state, lower_level_network_parameters, forward_caches)
-            error = batched_image[:, t] - predicted_frame
+            each_frame = batched_image[:, t]
 
-            # Store prediction error
-            pred_errors.append(np.mean(error**2))
+            predicted_frame = lower_network_forward(lower_level_state, lower_level_network_parameters, forward_caches)
+            avg_error, error = prediction_frame_error(predicted_frame, each_frame, forward_caches)
 
             # Update higher level
             higher_level_state = rnn_forward(error, higher_level_state, higher_rnn_parameters, forward_caches)
 
             # Generate transition weights
             w = hyper_network_forward(higher_level_state, hyper_network_parameters, forward_caches)
-            value = sum(w[:, k].reshape(-1, 1, 1) * Vk_parameters[k] for k in range(torch_model.K))
+            value = combine_transitions(w, Vk_parameters, forward_caches)
 
             # Update lower state with ReLU and noise
             lower_level_state_reshaped = np.expand_dims(lower_level_state, axis=-1)
@@ -153,8 +188,11 @@ def numpy_dpc(torch_model):
 
             # Collect digit logits
             model_prediction = classifier_forward(lower_level_state, digit_classifier_parameters, forward_caches)
+
             digit_logits.append(model_prediction)
-        
+            # Store frame prediction error
+            pred_errors.append(avg_error)
+
         model_digit_prediction = np.stack(digit_logits).mean(0)
         prediction_error = np.stack(pred_errors).mean()
 
